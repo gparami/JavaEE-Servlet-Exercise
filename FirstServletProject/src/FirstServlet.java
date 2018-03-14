5061b78a2b0e8dd125c228963eea0c26f4de450f
Parami Gunasekara <parami@live.ca>
2018-03-14 14:07:20 -0400

    testing html

--- FirstServletProject/src/FirstServlet.java
+++ FirstServletProject/src/FirstServlet.java
@@ -35,7 +35,6 @@ public class FirstServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		Date date = new Date();
-		out.println(HTML_START + "<h2>Hi There Artem!</h2><br/><h3>Date="+ date +"</h3>"+HTML_END);
 		out.println(HTML_START + "<h2>Hi There Artem!</h2><br/><h3>Date="+ date +"</h3><br/><button type=\"button\">Click Me!</button>"+HTML_END);
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
